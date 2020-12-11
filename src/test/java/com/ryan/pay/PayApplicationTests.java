60e1f93d227a3fff03bc028ab1a9bd9a12fc9327
ShuoNPS <shuoliu1122@gmail.com>
2020-12-10 21:35:39 -0800

    First commit

---
+++ src/test/java/com/ryan/pay/PayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
